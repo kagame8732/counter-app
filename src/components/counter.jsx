8d13f82e39f92d3c370795f24c955b79346da5f4
kagame8732 <kagame8732@gmail.com>
2021-08-13 07:06:46 +0200

    another commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,23 +2,28 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {  
-        count: 0
+        count: this.props.value
     };
-     handleIncrement =() => {
+    handleIncrement = (product) => {
+         console.log(product)
          this.setState({count:this.state.count + 1})
+     }
+    doHandleIncrement = () => {
+        this.handleIncrement({id: 1 })
     }
     render() {
-        return (
-        
-        <React.Fragment>
+        return ( 
+            <div>
             <span className="btn btn-info m-2">{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className="btn btn-secondary sm">Increment</button>  
-        </React.Fragment>
+                <button onClick=
+                    {this.doHandleIncrement} className="btn btn-secondary sm">Increment</button>
+                <button onClick=
+                {() => this.props.onDelete(this.props.id)} className="btn btn-danger sm m-2">Delete</button>
+        </div>
         )
     }
     formatCount(){
         return (this.state.count === 0 ? "zero" : this.state.count )
     }
 }
- 
 export default Counter;
